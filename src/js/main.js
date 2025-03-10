89de72e8a30341e8e94a58d3c000ddf397d51a2d
unknown <d.kazarov1361@gmail.com>
2025-03-10 21:48:19 +0200

    fix style

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,11 @@
-import { burgerOpen, burgerClose } from './js-components/burger';
 import { fetchData, renderData } from './js-components/fetchAndRenderData';
 import { deleteAction } from './js-components/deleteItem';
 import { postData } from './js-components/postData';
 import { updateItem } from './js-components/updateItem';
 import { getRandomGame } from './js-components/randomItem';
 
-
 // Случайная игра
 getRandomGame();
 
 // Видалити
 deleteAction();
-
-burgerOpen();
-burgerClose();
